7f47e72e6bd3a8e7cd61e1a6a5799e786b0b53f6
Abhilash Kumar <apravink@gmail.com>
2018-05-15 19:58:11 -0400

    Added dotenv

--- src/services/vision-service/vision-service.js
+++ src/services/vision-service/vision-service.js
@@ -1,5 +1,5 @@
-const config = require('../../../config');
 const axios = require('axios');
+require('dotenv').config();
 
 exports.callVisionApi = base64Image => {
   if (!base64Image || typeof base64Image !== 'string') {
@@ -20,28 +20,25 @@ exports.callVisionApi = base64Image => {
     ]
   };
 
-  return (
-    axios
-      .post(
-        `https://vision.googleapis.com/v1/images:annotate?key=${
-          config.GOOGLE_VISION_API_KEY
-        }`,
-        {
-          ...requestBody
-        }
-      )
-      // .then(response => console.log(response.data.responses[0]))
-      .then(response => {
-        if (response.data.responses[0].error) {
-          return response.data.responses[0];
-        }
+  return axios
+    .post(
+      `https://vision.googleapis.com/v1/images:annotate?key=${
+        process.env.GOOGLE_VISION_API_KEY
+      }`,
+      {
+        ...requestBody
+      }
+    )
+    .then(response => {
+      if (response.data.responses[0].error) {
+        return response.data.responses[0];
+      }
 
-        return response.data.responses[0].fullTextAnnotation.text
-          .split(',')
-          .map(ingredient => {
-            return ingredient.replace(/r\n|\r|\n|$/, '');
-          });
-      })
-      .catch(error => console.log('error', error))
-  );
+      return response.data.responses[0].fullTextAnnotation.text
+        .split(',')
+        .map(ingredient => {
+          return ingredient.replace(/r\n|\r|\n|$/, '');
+        });
+    })
+    .catch(error => console.error('error', error));
 };
